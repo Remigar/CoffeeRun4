e4379fab890b4414855822087356476da6a5ab2e
Eric Lara <elara9000@gmail.com>
2017-03-29 19:46:38 -0700

    Quick Correction

--- scripts/validation.js
+++ scripts/validation.js
@@ -29,7 +29,7 @@
             });
 
             console.log(result);
-            if (result.status == '{}')
+            if (result.responseText == '{}')
                 return false;
             else return true;
         }
